0436f3d02e635422c0c6eb5f33a5129266339d54
xuliangzhan <xu_liangzhan@163.com>
2019-06-06 22:06:40 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve (dir) {
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
+  publicPath: '/',
   outputDir: 'docs',
   assetsDir: 'static',
   productionSourceMap: false,
